ef564f4faa0f81265c5a67f91dfd43ca9873eb7f
Matheus Loubach <34719454+Matheus-Loubach@users.noreply.github.com>
2022-08-25 20:42:08 -0300

    Update GamerOver.jsx

--- src/components/GamerOver.jsx
+++ src/components/GamerOver.jsx
@@ -11,7 +11,8 @@ const GamerOver = ({menu}) => {
       <h1 className="titulo_menu">Não deixe que sua pontuação fique abaixo de ZERO.</h1>
       <h2 className="sub_menu">Fim de Jogo</h2>
       <button className="botao_final" onClick={menu}>Menu Principal</button>
-   
+     <footer className="sub_menu"> 
+Developed by - Matheus Loubach <footer/>
       </div>
     </div>
   )
